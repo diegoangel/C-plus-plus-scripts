07c64e6072fb35f5da68e76b3889e8aea1c00b83
Diego Rivarola <diegoangel77@gmail.com>
2015-08-05 19:55:53 -0300

    Some new changes

--- test_vector.cpp
+++ test_vector.cpp
@@ -7,8 +7,8 @@ int main(int argc, char* argv[])
     char c[] = "tu vieja en tanga";
     std::vector<char> v;
     v.push_back(c);
-
-    std::cout << v[0] << std::endl;
+    std::string str = v[0]; 
+    std::cout << str << std::endl;
 
     return 0;
 }
